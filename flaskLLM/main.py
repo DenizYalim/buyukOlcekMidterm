cb7ebe3458500ea15de3438d91dd4f188494daf9
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-05-22 04:54:10 +0300

    initial gateway setup

--- flaskLLM/main.py
+++ flaskLLM/main.py
@@ -1,1 +1,16 @@
 from flask import request, jsonify
+from llm import getResponse
+
+app = Flask(__name__)
+
+print(getResponse("hello gpt what's 5+5"))
+
+@app.route("/getResponse", methods=["GET"])
+def get_recipeList():
+    message = request.params.get("message")
+    message = request.params.get("context")
+    return getResponse(message, context)
+
+
+if __name__ == "__main__":
+    app.run(port=8000, debug = True)
